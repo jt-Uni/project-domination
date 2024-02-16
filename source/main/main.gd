0bd188f139bfabd71e555fdcad8ab276d97f3b31
James Taylor <James.steve.taylor@gmail.com>
2024-02-16 13:20:25 +0000

    Setting up main menu

---
+++ source/main/main.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+func _on_play_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_options_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
