eb0e9d47eb856b503f70f69d3a4bdaf3eb98d8b5
James Taylor <James.steve.taylor@gmail.com>
2024-02-19 17:53:03 +0000

    Start creating country polygons

---
+++ scenes/main/main.gd
@@ -0,0 +1,14 @@
+extends Control
+
+@onready var load_scene = load("res://scenes/gameplay/game.tscn")
+
+func _on_play_button_pressed() -> void:
+	var this_scene = load_scene.instantiate()
+	get_tree().root.add_child(this_scene)
+	queue_free()
+
+func _on_options_button_pressed():
+	pass # Replace with function body.
+
+func _on_quit_button_pressed():
+	get_tree().quit()
