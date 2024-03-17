c93cf5cea028be82d553bbc5f7dffe2370a223e9
James Taylor <James.steve.taylor@gmail.com>
2024-03-17 12:12:34 +0000

    Finished the polygons for south and north america

--- Scripts/Map.gd
+++ Scripts/Map.gd
@@ -1,9 +1,10 @@
 extends Area2D
 
 
-func _on_Area2D_mouse_entered():
-	modulate = Color(0, 1, 0)
+
+func _on_mouse_entered():
+	modulate = Color(0, 0, 1)
 
 
-func _on_Area2D_mouse_exited():
+func _on_mouse_exited():
 	modulate = Color(1, 1, 1)
--- Scripts/Menu/Main.gd
+++ Scripts/Menu/Main.gd
@@ -1,17 +1,17 @@
 extends Control
 
 # replace with correct file directory
-@onready var load_scene = load("")
+@onready var load_scene = load("res://Scenes/map.tscn")
 
 func _on_play_button_pressed() -> void:
 	
-	#var this_scene = load_scene.instantiate()
-	#get_tree().root.add_child(this_scene)
-	#queue_free()
-	
-	var menuplayerscreen = load("res://Scenes/Menu/Menuchooseplayers.tscn").instantiate()
-	get_tree().root.add_child(menuplayerscreen)
+	var this_scene = load_scene.instantiate()
+	get_tree().root.add_child(this_scene)
 	queue_free()
+	
+	#var menuplayerscreen = load("res://Scenes/Menu/Menuchooseplayers.tscn").instantiate()
+	#get_tree().root.add_child(menuplayerscreen)
+	#queue_free()
 	
 func _on_options_button_pressed():
 	pass # Replace with function body.
