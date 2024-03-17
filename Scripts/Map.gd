fde53a3a43fe850e315298cc8c30cf67739a43a5
James Taylor <James.steve.taylor@gmail.com>
2024-03-17 11:49:59 +0000

    map progress
    + Created north America image
    + Create north America Collison polygons
    + Created south America image

--- Scripts/Map.gd
+++ Scripts/Map.gd
@@ -1,34 +1,9 @@
 extends Area2D
 
-var region_name = ""
 
-func _ready():
-	pass
-
-
-func _process(delta):
-	pass
+func _on_Area2D_mouse_entered():
+	modulate = Color(0, 1, 0)
 
 
-func _on_child_entered_tree(node):
-	if node.is_class("Polygon2D"):
-		node.color = Color(1,1,1,0.5)
-
-
-func _on_mouse_entered():
-	print(region_name)
-	for node in get_children():
-		if node.is_class("Polygon2D"):
-			node.color = Color(1,1,1,1)
-	
-
-
-func _on_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
-		print(str(region_name) + " Clicked")
-
-
-func _on_mouse_exited():
-	for node in get_children():
-		if node.is_class("Polygon2D"):
-			node.color = Color(1,1,1,0.5)
+func _on_Area2D_mouse_exited():
+	modulate = Color(1, 1, 1)
--- Scripts/Menu/Main.gd
+++ Scripts/Menu/Main.gd
@@ -1,14 +1,15 @@
 extends Control
 
-@onready var load_scene = load("res://Scenes/Gameplay.tscn")
+# replace with correct file directory
+@onready var load_scene = load("")
 
 func _on_play_button_pressed() -> void:
 	
 	#var this_scene = load_scene.instantiate()
 	#get_tree().root.add_child(this_scene)
 	#queue_free()
+	
 	var menuplayerscreen = load("res://Scenes/Menu/Menuchooseplayers.tscn").instantiate()
-	#var scene = menuplayerscreen.instantiate()
 	get_tree().root.add_child(menuplayerscreen)
 	queue_free()
 	
